06a07c28c04b6bb70eb7e7f26189987348a98a09
JuanFelipeLedesma <j.ledesma@uniandes.edu.co>
2023-03-29 11:08:55 -0500

    fue hecho metodo realizarReserva

--- Entrega3/P1-entrega 3/src/Modelo/Recepcionista.java
+++ Entrega3/P1-entrega 3/src/Modelo/Recepcionista.java
@@ -1,7 +1,10 @@
 package Modelo;
+import java.util.*;
 
 public class Recepcionista {
     
+    private List<Registro_Retiro> registro_Retiro;
+    private List<Habitacion> habitaciones;
     
     
     public void consultarInventario() {
@@ -24,10 +27,40 @@ public class Recepcionista {
         
     }
     
-    public void realizarReserva() {
-        
+    public Recepcionista() {
+        registro_Retiro = new ArrayList<>();
+        habitaciones = new ArrayList<>();
     }
-    
+
+    public boolean realizarReserva(String nombre, int documento, String email, int celular, int totalPersonas, String fechaInicio, String fechaFin, int numeroMenores) {
+        List<Habitacion> habitacionesDisponibles = new ArrayList<>();
+        for (Habitacion habitacion : habitaciones) {
+            boolean disponible = true;
+            for (Registro_Retiro registro : registro_Retiro) {
+                if (registro.habitacionOcupada(habitacion.getId(), fechaInicio, fechaFin)) {
+                    disponible = false;
+                    break;
+                }   
+            }
+            if (disponible) {
+                habitacionesDisponibles.add(habitacion);
+            }
+        }
+
+        if (habitacionesDisponibles.isEmpty()) {
+            return false; // Osea no hay habitaciones disponibles
+        }
+
+        Habitacion habitacionSeleccionada = habitacionesDisponibles.get(0);
+
+        Registro_Retiro nuevoRegistro = new Registro_Retiro(nombre, documento, email, celular, totalPersonas, true, false, habitacionSeleccionada.getId(), numeroMenores, fechaInicio, fechaFin);
+        registro_Retiro.add(nuevoRegistro);
+    return true;
+
+    }
+
+
+
     public void cancelarReserva() {
         
     }
--- Entrega3/P1-entrega 3/src/Modelo/Registro_Retiro.java
+++ Entrega3/P1-entrega 3/src/Modelo/Registro_Retiro.java
@@ -10,8 +10,10 @@ public class Registro_Retiro {
     private boolean salida;
     private String identificador;
     private int numeroMenores;
+    private String fechaInicio;
+    private String fechaFin;
     
-    public Registro_Retiro(String nombreHuesped, int documento, String correo, int numeroCelular, int personasEsperadas, boolean registro, boolean salida, String identificador, int numeroMenores) {
+    public Registro_Retiro(String nombreHuesped, int documento, String correo, int numeroCelular, int personasEsperadas, boolean registro, boolean salida, String identificador, int numeroMenores, String fechaInicio, String fechaFin) {
         this.nombreHuesped = nombreHuesped;
         this.documento = documento;
         this.correo = correo;
@@ -21,6 +23,8 @@ public class Registro_Retiro {
         this.salida = salida;
         this.identificador = identificador;
         this.numeroMenores = numeroMenores;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
     }
     
     public void generarRegistro() {
@@ -40,4 +44,24 @@ public class Registro_Retiro {
         
         return 0.0f;
     }
+
+    public boolean habitacionOcupada(String idHabitacion, String fechaInicioConsulta, String fechaFinConsulta) {
+        if (this.identificador.equals(idHabitacion)) {
+            //Fechas están en formato "YYYY-MM-DD"
+            int inicioActual = Integer.parseInt(this.fechaInicio.replace("-", ""));
+            int finActual = Integer.parseInt(this.fechaFin.replace("-", ""));
+            int inicioConsulta = Integer.parseInt(fechaInicioConsulta.replace("-", ""));
+            int finConsulta = Integer.parseInt(fechaFinConsulta.replace("-", ""));
+
+            // Comprobar si hay sobreposición entre las fechas
+            //Si se sobreponen el método retorna true indicando que la habitación está ocupada en el rango de fechas
+            if ((inicioConsulta >= inicioActual && inicioConsulta <= finActual) ||
+                (finConsulta >= inicioActual && finConsulta <= finActual) ||
+                (inicioConsulta <= inicioActual && finConsulta >= finActual)) {
+                return true; // La habitación está ocupada en el rango de fechas especificado
+            }
+        }
+        return false; // La habitación no está ocupada en el rango de fechas especificado
+    }
 }
+
